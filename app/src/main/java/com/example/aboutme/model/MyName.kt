9f1fcb5ae010571d4daf417dc1ed1efee7516efd
saurabh-somani <SaurabhNarendra.Somani@philips.com>
2021-07-20 13:12:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aboutme/model/MyName.kt
@@ -0,0 +1,13 @@
+package com.example.aboutme.model
+
+/**
+ * Project          : About Me
+ * Revision History : version 1
+ * Date             : 30/05/21
+ * Original author  : saurabh
+ * Description      : Initial version
+ */
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)
